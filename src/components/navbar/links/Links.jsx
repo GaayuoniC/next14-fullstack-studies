867d280362a1c132f1c3e44db64e540cbd8cb7b7
christian <gayuoni@proton.me>
2024-05-27 11:18:44 +0200

    Responsive design 50 percent completed

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,9 +1,14 @@
 
-import Link from "next/link"
+"use client"
+import { useState } from "react"
 import styles from "./linkscss/links.module.css"
 import NavLink from "./navLink/navLink"
 
 const Links = ()=>{
+    //Using use state to manage the responsiveness of the menu bar
+    const [open,setOpen]= useState(false)
+
+
 
     //Create an array of links here:
     const links =[
@@ -24,14 +29,45 @@ const Links = ()=>{
             path:"/blog"
         }
     ]
+//Temporary data structure here!!
+    const session = true
+    const isAdmin = false
 
 //Map the array for the links below in the return part
-    return( <div className={styles.links} >
+    return(
+       <div className= {styles.container}> 
+        
+        
+        <div className={styles.links} >
         
         {
 
             links.map((link=>(<NavLink item= {link} key={link.title} />) ))
         }
+        {/* Set the conditions for displaying admin or user here::: */}
+        {
+            session ? (
+                <> 
+                 {
+                    isAdmin && (
+                        <NavLink item={{title:"Admin", path:"/admin"}}/> 
+                    )
+                } 
+                <button className= {styles.logout}>Logout</button>
+                </>
+                ): (
+                <NavLink item={{title:"Login", path:"/login"}}/>
+            )
+        }
+    </div> 
+    {/* Implementing the use state on the button with an onclick event */}
+    <button className={styles.menuButton} onClick={()=>setOpen((prev)=>!prev)}>Menu</button>
+    {
+        open && <div className={styles.mobileLinks}>
+            {links.map((link)=>(<NavLink item= {link} key={link.title}  />) )}
+        </div>
+    }
+    
     </div>)
 }
 export default Links
--- src/components/navbar/links/navLink/navLink.jsx
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -10,13 +10,19 @@ import { usePathname } from "next/navigation"
 
 const NavLink = ({item})=>{
     //Use the path name hook
-    const pathName = usePathname
+    const pathName = usePathname()
 
     return(
-        <div className=   {` ${styles.container}  ${pathName === item.path ? styles.active : ''} `} >
-            <Link href={item.path} > {item.title} </Link>
+        // <div className=   {` ${styles.container}  ${pathName === item.path ? styles.active : ''} `} >
+        //     <Link href={item.path} > {item.title} </Link>
 
-        </div>
+        // </div>
+      
+            <Link href={item.path} className={`${styles.container} 
+            ${pathName === item.path && styles.active }`} >
+                {item.title}
+                </Link>
+      
 
     )
 }
